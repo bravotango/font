6efa61c1d0529d8ba46b2d6484980b6d167006fc
Bravo "Sasquatch" Tango <seattleriot@hotmail.com>
2018-01-31 00:08:59 -0800

    Navigation update - new "number" button function to filter on font names starting with a number

--- src/app/font.service.ts
+++ src/app/font.service.ts
@@ -6,6 +6,7 @@ import { of } from 'rxjs/observable/of';
 import { Font } from './font';
 import { FONTS } from './mock-fonts';
 import { MessageService } from './message.service';
+import { notEqual } from 'assert';
 
 @Injectable()
 
@@ -25,17 +26,28 @@ export class FontService {
   getList(string): Observable<Font[]> {
     // Todo: send the message _after_ fetching the fonts
     this.messageService.add('getList: entered');
-	
-	  this.filteredFonts = this.filterFonts(string);
- 
+  
+    if (string.length==1) {
+	  this.filteredFonts = this.filterFontsByCaseSensitiveSingleCharacter(string);
+    } 
+    if (string=="NUMBER") {
+      this.filteredFonts = this.filterFontsByStartsWithNumber();
+      } 
+
     return of(this.filteredFonts);
   }
 
-  filterFonts(string:string){
+  filterFontsByCaseSensitiveSingleCharacter(string:string){
     //if font.name starts with matching parameter 'char' add to array
-    this.messageService.add('filterFonts: filtered by ' + string);
+    this.messageService.add('filterFontsByCaseSensitiveSingleCharacter: filtered by ' + string);
     return FONTS.filter(FONTS => FONTS.name[0].toUpperCase().indexOf(string) !== -1);
 
- //   return (fFonts)
+  }
+
+  filterFontsByStartsWithNumber(){
+    //if font.name starts with matching parameter 'char' add to array
+    this.messageService.add('filterFontsByStartsWithNumber: filtered by a number');
+    return FONTS.filter(FONTS => FONTS.name[0].match(/^\d/));
+
   }
 }
