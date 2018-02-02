e0c0250c132205e6ba6770625aac39b0e3e0776c
Bravo "Sasquatch" Tango <seattleriot@hotmail.com>
2018-02-01 22:04:31 -0800

    Clean-up, removed navigation & route junk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,9 @@ import { ListComponent }      from './list/list.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'list', component: ListComponent },
-  { path: 'list/:char', component: ListComponent }
+  { path: '', redirectTo: '/list/random', pathMatch: 'full' },
+  { path: 'list/:char', component: ListComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
